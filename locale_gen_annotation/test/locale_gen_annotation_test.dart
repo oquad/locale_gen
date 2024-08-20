4d9ee672c3f151efbbbf1993a21ab3f68e12253b
Alex Bukin <bukin.alexey.a@gmail.com>
2024-08-20 22:59:35 +0300

    Flutter compatibility fixes

--- locale_gen_annotation/test/locale_gen_annotation_test.dart
+++
@@ -1,8 +0,0 @@
-import 'package:locale_gen_annotation/locale_gen_annotation.dart';
-import 'package:test/test.dart';
-
-void main() {
-  test('calculate', () {
-    expect(calculate(), 42);
-  });
-}
--- locale_generator/CHANGELOG.md
+++ locale_generator/CHANGELOG.md
@@ -1,3 +1,7 @@
 ## 1.0.0
 
 - Initial version.
+
+## 1.0.6
+
+- Flutter compatibility fixes
